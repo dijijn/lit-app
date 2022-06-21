e79800a6c88f9537e96be514ab1e1619645e7d00
dijijn <nunes.jonathan@aluno.ifsp.edu.br>
2022-06-21 18:51:32 -0300

    update element

--- src/lit-app.ts
+++ src/lit-app.ts
@@ -8,7 +8,17 @@ export class LitApp extends LitElement {
             :host {
                 display: flex;
             }
-            div{
+
+            .master{
+                display: flex;
+                flex-direction: column;
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+                width: 100%;
+                height: auto;
+            }
+            .navbar{
                 display: flex;
                 width: 100%;
                 height: 50px;
@@ -17,7 +27,7 @@ export class LitApp extends LitElement {
                 justify-content: space-around;
                 align-items: center;
             }
-            p{
+            .pnav{
                 padding: 5px;
                 background-color: lightblue;
                 border-radius: 10px;
@@ -25,19 +35,57 @@ export class LitApp extends LitElement {
                 font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
             }
 
-            input{
+            .navinput{
                 height: 20px;
             }
+            .item{
+                display: flex;
+                justify-content: space-around;
+                align-items: center;
+                margin-top: 20px;
+            }
+
         `
     ];
 
     @property() name = 'Lit App';
 
     render() {
-        return html`<div>
-        <p>HOME</p>
-        <input placeholder='pesquisa'></input>
-        <p>MINHA CONTA</p>
-    </div>`;
+        return html`
+        
+    <div class='master'>
+    <div class='navbar'>
+        <p class='pnav'>HOME</p>
+        <input class='navinput' placeholder='pesquisa'></input>
+        <p class='pnav'>MINHA CONTA</p>
+    </div>
+    
+    <div class='item'>
+        <div class='imagem'>
+            <img src='/' width=80% height=100px/> 
+        </div>
+        <div class='qtd'>
+            <input class='inqtd' placeholder='Quantidade'></input>
+        </div>
+        <div class='preco'>
+            <p class='price'>R$ 50,00</p>
+        </div>
+    </div>
+    <div class='item'>
+        <div class='imagem'>
+            <img src='/' width=80% height=100px/> 
+        </div>
+        <div class='qtd'>
+            <input class='inqtd' placeholder='Quantidade'></input>
+        </div>
+        <div class='preco'>
+            <p class='price'>R$ 50,00</p>
+        </div>
+    </div>
+    </div>
+    `;
+    
+
+    
     }
 }
