fdf3c8e3e4cb239b7a82a529136c088e77ae51c0
dijijn <nunes.jonathan@aluno.ifsp.edu.br>
2022-06-22 13:53:06 -0300

    cart finalizado

--- src/lit-app.ts
+++ src/lit-app.ts
@@ -37,12 +37,69 @@ export class LitApp extends LitElement {
 
             .navinput{
                 height: 20px;
+                text-align: center;
+            }
+            .items{
+                flex: 1;
             }
             .item{
                 display: flex;
                 justify-content: space-around;
                 align-items: center;
                 margin-top: 20px;
+            }
+            .pedidos{
+                flex: 1;
+            }
+            .resumo{
+                display: flex;
+                width: 100%;
+                justify-content: space-around;
+                background-color: aliceblue;
+                flex-wrap: wrap;
+                margin-top: 20px;
+                border-radius: 5px;
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            }
+            .valor{
+                display: flex;
+                width: 100%;
+                padding-left: 10px;
+                padding-right: 10px;
+                justify-content: space-between;
+            }
+            .bt{
+                padding: 10px;
+                border-radius: 5px;
+                background-color: lightblue;
+                border: none;
+            }
+            .bt:hover{
+                background-color: aqua;
+            }
+
+            @media screen and (min-width: 900px) {         
+                
+                .items{
+                    display: flex;
+                    width: 100%;
+                    background-color: red;
+                    flex: 4;
+                    flex-direction: column;
+                }
+                .pedidos{
+                    display: flex;
+                    width: 100%;
+                    flex: 1;
+                }
+                .btn{
+                    display:flex;
+                    justify-content: flex-end;
+                    align-items: flex-end;
+                    width: 100%;
+                    margin-top: 30px;
+                    padding: 10px;
+                }
             }
 
         `
@@ -54,34 +111,63 @@ export class LitApp extends LitElement {
         return html`
         
     <div class='master'>
-    <div class='navbar'>
-        <p class='pnav'>HOME</p>
-        <input class='navinput' placeholder='pesquisa'></input>
-        <p class='pnav'>MINHA CONTA</p>
-    </div>
-    
-    <div class='item'>
-        <div class='imagem'>
-            <img src='/' width=80% height=100px/> 
+        <div class='navbar'>
+            <p class='pnav'>HOME</p>
+            <input class='navinput' placeholder='pesquisa'></input>
+            <p class='pnav'>MINHA CONTA</p>
         </div>
-        <div class='qtd'>
-            <input class='inqtd' placeholder='Quantidade'></input>
+        <div class='items'>
+            <div class='item'>
+                <div class='imagem'>
+                    <img src='/' width=80% height=100px/> 
+                </div>
+                <div class='qtd'>
+                    <input class='inqtd' placeholder='Quantidade'></input>
+                </div>
+                <div class='preco'>
+                    <p class='price'>R$ 50,00</p>
+                </div>
+            </div>
+            <div class='item'>
+                <div class='imagem'>
+                    <img src='/' width=80% height=100px/> 
+                </div>
+                <div class='qtd'>
+                    <input class='inqtd' placeholder='Quantidade'></input>
+                </div>
+                <div class='preco'>
+                    <p class='price'>R$ 50,00</p>
+                </div>
+            </div>
         </div>
-        <div class='preco'>
-            <p class='price'>R$ 50,00</p>
-        </div>
-    </div>
-    <div class='item'>
-        <div class='imagem'>
-            <img src='/' width=80% height=100px/> 
-        </div>
-        <div class='qtd'>
-            <input class='inqtd' placeholder='Quantidade'></input>
-        </div>
-        <div class='preco'>
-            <p class='price'>R$ 50,00</p>
-        </div>
-    </div>
+        <div class='pedidos'>
+            <div class='resumo'>
+                <div>
+                    <span>Resumo do Pedido:</span>
+                </div>
+                <div class='valor'>
+                    <p>item</p>
+                    <p>R$50,00</p>
+                </div>
+                <div class='valor'>
+                    <p>item</p>
+                    <p>R$50,00</p>
+                </div>
+                <div class='valor'>
+                    <p>Frete</p>
+                    <p>Grátis</p>
+                </div>
+                <div class='valor'>
+                    <p>Subtotal</p>
+                    <p>R$100,00</p>
+                </div>
+                <div class='btn'>
+                    <button class='bt'>
+                        Finalizar compra
+                    </button>
+                </div>
+            </div>
+        </div>      
     </div>
     `;
     
